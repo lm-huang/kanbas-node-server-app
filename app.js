4724c9235e958c2fa4e3577eddb9298199ef5afd
Liming <huangliming1000@163.com>
2023-12-04 19:40:28 -0500

    change

--- app.js
+++ app.js
@@ -14,21 +14,26 @@ import session from "express-session"
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+));
 const sessionOptions = {
     secret: "any string", resave: false, saveUninitialized: false,
 };
-if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none", secure: true,
-    };
-}
+// if (process.env.NODE_ENV !== "development") {
+//     sessionOptions.proxy = true;
+//     sessionOptions.cookie = {
+//         sameSite: "none", secure: true,
+//     };
+// }
+app.use(session(sessionOptions));
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 
-app.use(session(sessionOptions));
+
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
